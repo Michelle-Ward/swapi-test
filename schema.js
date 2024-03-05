8fd0bbc29f89352d061d3292ee0a106d83c82153
Michelle-Ward <mward352@gmail.com>
2024-03-04 20:40:31 -0800

    add people schema

---
+++ schema.js
@@ -0,0 +1,22 @@
+
+
+// create a struct where I can compare to 
+// make value the type
+const People = {
+    name: "string",
+    birth_year: "string",
+    eye_color: "string",
+    gender: "string", // enum Male, Female, unkown "n/a"
+    hair_color: "string", 
+    height: "string", // TEST: is it in centimeters?
+    mass: "string", // TEST: is it formatted in kilograms
+    skin_color: "string",
+    homeworld: "string", //TEST: is this a valid planet resource
+    films: "array", //Test: is this a valid array of film urls
+    species: "array", //""
+    starships: "array", //""
+    vehicles: "array", //""
+    url: "string", // TEST does this match the url we used?
+    created: "string", //TEST valid date?
+    edited: "string", //""
+}
